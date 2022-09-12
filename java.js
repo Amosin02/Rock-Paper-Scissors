370396abf165993efb89527440f197aba5fa15ad
Nhoj <amosinnhoj@gmail.com>
2022-09-12 09:25:38 +0800

    Finding the winner done

--- java.js
+++ java.js
@@ -4,22 +4,49 @@ function getComputerchoice(){
     return random;
 }
 
-const playerSelection = "rock";
+console.log(getComputerchoice())
+
+const playerSelection = "Rock";
 const computerSelection = getComputerchoice();
+console.log(playRound(playerSelection, computerSelection))
 
 function playRound(playerSelection, computerSelection){
  if (playerSelection == "rock"){
-    if (getComputerchoice === 2){
-        console.log("lose");
+    if (getComputerchoice() === 2){
+        console.log("You Win! Rock beats Scissors (scissors)");
     }
-    else if(getComputerchoice === 1){
-        console.log("win");
+    else if(getComputerchoice() === 1){
+        console.log("You Lose! Paper beats Rock (paper)");
     }
     else{
-        console.log("tie");
+        console.log("It's a Tie! You both pick Rock (rock)");
+    }
+ }
+ else if(playerSelection == "paper"){
+    if (getComputerchoice() === 2){
+        console.log("You Lose! Scissors beats Paper (scissors)");
+    }
+    else if(getComputerchoice() === 1){
+        console.log("It's a Tie! You both pick Paper (paper)");
+    }
+    else{
+        console.log("You Win! Paper beats Rock (rock)");
+    }
+ }
+ else if(playerSelection == "scissors"){
+    if (getComputerchoice() === 2){
+        console.log("It's a Tie! You both pick Scissors (scissors)");
+    }
+    else if(getComputerchoice() === 1){
+        console.log("You Win! Scissors beats Paper (paper)");
+    }
+    else{
+        console.log("You Lose! Rock beats Scissors (rock)");
     }
  }
 }
+
+
 
 //computer pick a random number 1-35 36-67 68-100
 //0=rock 1=paper 2=scissors
